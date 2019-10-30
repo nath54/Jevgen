5fa942fe6657b532bdf223dacce069ab6031dcbe
nath54 <cerisara.nathan@gmail.com>
2019-10-30 11:25:35 +0100

    a

--- main.js
+++ main.js
@@ -1,37 +1,39 @@
 canvas=document.getElementById("canvas");
 ctx=canvas.getContext("2d");
 
-var tex=700;
-var tey=700;
+var tex=parseInt(canvas.width);
+var tey=parseInt(canvas.height);
 
 var dt=new Date();
 var dbg=dt.getTime();
 var tbg=10;
 
-var mape={nom:"mape",rebond_bords=true,obstacles=[],objectif=[],pstart=[]};
+var mape={nom:"mape",rebond_bords:true,obstacles:[],objectif:[],pstart:[]};
 var persos=[];
 var nbp=30;
-var nbo=5;
-var to=100;
+var nbo=40;
+var to=50;
 var tp=25;
 
-var bvit=0.0001;
-var bcvit=0.01;
+var bvit=0.1;
+var bcvit=1;
 
 var nbgen=1;
 var encour=true;
 var fini=false;
 
-function collide(r1,r2){
-	return rect1.x < rect2.x + rect2.tx && rect1.x + rect1.tx > rect2.x && rect1.y < rect2.y + rect2.ty && rect1.ty + rect1.y > rect2.y;
+var ps={};
+
+function collide(rect1,rect2){
+	return rect1.px < rect2.px + rect2.tx && rect1.px + rect1.tx > rect2.px && rect1.py < rect2.py + rect2.ty && rect1.ty + rect1.py > rect2.py;
 }
 
 function d_collide(r,rr){
 	var sens=[];
-	if(r.py < rr.py ) sens.push("up");
-	if(r.py+r.ry > rr.py+rr.ty) sens.push("down");
-	if(r.px < rr.px ) sens.push("left");
-    if(r.px+r.tx > rr.px+rr.tx ) sens.push("right");
+	if(r.py < rr.py && r.py+r.ty>rr.py) sens.push("up");
+	if(r.py+r.ty > rr.py+rr.ty && r.py < rr.py+rr.ty) sens.push("down");
+	if(r.px < rr.px && r.px+r.tx>rr.px) sens.push("left");
+    if(r.px+r.tx > rr.px+rr.tx && r.px < rr.px+rr.tx ) sens.push("right");
 	return sens
 }
 
@@ -39,12 +41,12 @@ function dist(p,pp){ return Math.sqrt((p.px-pp.px)**2+(p.py-pp.py)**2) }
 
 function gen(){
 	mape.objectif={px:Math.random()*(tex-50),py:Math.random()*(tey-50),tx:50,ty:50};
-    Mape.pstart=[50+Math.random()*(tex-100),50+Math.random()*(tey-100)];
+    mape.pstart=[50+Math.random()*(tex-100),50+Math.random()*(tey-100)];
 	mape.obstacles=[];
 	for(x=0;x<nbo;x++){
 		var xx=Math.random()*tex;
 		var yy=Math.random()*tey
-		while(collide({px:xx,py:yy,tx:to,ty:to},mape.objectif) && collide({px:xx,py:yy,tx:to,ty:to},{px:mape.pstart[0]-50,py:mape.pstart-50,tx:100,ty:100}){
+		while( collide( {px:xx,py:yy,tx:to,ty:to} , mape.objectif ) && collide( {px:xx,py:yy,tx:to,ty:to} , {px:mape.pstart[0]-50,py:mape.pstart-50,tx:100,ty:100} )){
 			var xx=Math.random()*tex;
 		    var yy=Math.random()*tey;
 		}
@@ -53,16 +55,23 @@ function gen(){
 	for( x=0; x<nbp ; x++){
 		var vx=(-1000.0+Math.random()*2000.0)/100.0;
 		var vy=(-1000.0+Math.random()*2000.0)/100.0;
-		persos.push({px:mape.pstart[0],py:mape pstart[1],tx:tp,ty:tp,vitx:vx,vity:vy,bvitx:vx,bvity:vy})
+		persos.push( { px : mape.pstart[0] , py : mape.pstart[1] , tx:tp , ty:tp , vitx:vx , vity:vy , bvitx:vx , bvity:vy , ap:[] , cl:[0,0,255] } );
 	}
 }
 
 
 function ev(){
+    var vv=Object.keys(ps).sort()
+    var valeurs=[];
+    for(x=0;x<vv.length*5/100;x++){
+        valeurs.push(ps[vv]);
+    }
+    persos=[];
 	for( x=0; x<nbp ; x++){
-		var vx=(-1000.0+Math.random()*2000.0)/100.0;
-		var vy=(-1000.0+Math.random()*2000.0)/100.0;
-		persos.push({px:mape.pstart[0],py:mape pstart[1],tx:tp,ty:tp,vitx:vx,vity:vy,bvitx:vx,bvity:vy})
+	    vvv=valeurs[parseInt(Math.random()*valeurs.length)];
+		var vx=vvv[0]+(-100.0+Math.random()*200)/100.0;
+		var vy=vvv[1]+(-100.0+Math.random()*200)/100.0;
+		persos.push({px:mape.pstart[0],py:mape.pstart[1],tx:tp,ty:tp,vitx:vx,vity:vy,bvitx:vx,bvity:vy,ap:[],cl:[0,0,255]})
 	}
 	nbgen+=1;
 }
@@ -70,8 +79,17 @@ function ev(){
 function aff(){
 	ctx.fillStyle="rgb(255,255,255)";
 	ctx.fillRect(0,0,tex,tey);
-	ctx.fillStyle="rg (0,0,255)";
 	for( p of persos ){
+	    ctx.fillStyle="rgb("+p.cl[0]+","+p.cl[1]+","+p.cl[2]+")";
+    	ctx.strokeStyle="rgb("+p.cl[0]+","+p.cl[1]+","+p.cl[2]+")";
+	    if( p.ap.length > 1){
+            ctx.beginPath();
+            ctx.moveTo(p.ap[0][0],p.ap[0][1]);
+    	    for( a of p.ap ){
+	            ctx.lineTo(a[0],a[1]);
+	        }
+	        ctx.stroke();
+	    }
 		ctx.fillRect(p.px,p.py,p.tx,p.ty);
 	}
 	ctx.fillStyle="rgb(30,30,30)";
@@ -107,22 +125,44 @@ function update(){
 		for( o of mape.obstacles ){
 			if(collide(p,o)){
 				sens=d_collide(p,o);
-				if("up" in sens || "down" in sens){
+				if(sens.includes("up") || sens.includes("down")){
                     p.vity=-p.vity;
                     p.vity=baisseVit(p.vity,bcvit);
                     if("up" in sens) p.py=o.py-p.ty-1;
                     if("down" in sens) p.py=o.py+o.ty+1;
                 }
-				if("left" in sens || "right" in sens){
+				if(sens.includes("left") || sens.includes("right")){
 					p.vitx=baisseVit(p.vitx,bvit);
                     p.vitx=-p.vitx;
                     if("left" in sens) p.px=o.px-p.tx-1;
                     if("right" in sens) p.px=o.px+o.tx+1;
                 }
 			}
-			if(collide(p,mape.objectif)){
-				fini=true;
-			}
+		}
+		if(mape.rebond_bords){
+			 if(p.px<0){
+			    p.px=0;
+			    p.vitx=-p.vitx;
+			    p.vitx=baisseVit(p.vitx,bvit);
+			 }
+			 if(p.px+p.tx>tex){
+			    p.px=tex-p.tx;
+			    p.vitx=-p.vitx;
+			    p.vitx=baisseVit(p.vitx,bvit);
+			 }
+			 if(p.py<0){
+			    p.py=0;
+			    p.vity=-p.vity;
+			    p.vity=baisseVit(p.vity,bvit);
+			 }
+			 if(p.py+p.ty>tey){
+			    p.py=tey-p.ty;
+			    p.vity=-p.vity;
+			    p.vity=baisseVit(p.vity,bvit);
+			 }
+		}
+		if(collide(p,mape.objectif)){
+			fini=true;
 		}
 	}
 }
@@ -137,11 +177,12 @@ function main(){
 			update();
 			ps={}
 			for(p of persos){
+			    p.ap.push([p.px+p.tx/2,p.py+p.ty/2]);
 				if(p.vitx==0 && p.vity==0){
-					ps[ dist(p,mape.objectif) ]=[p.bvitx,p.bvity];
+					ps[dist(p,mape.objectif)]=[p.bvitx,p.bvity];
 				}
 			}
-			if(ps.length==persos.length){
+			if(Object.keys(ps).length==persos.length){
 				setTimeout( ev , 1000 );
 			}
 		}
